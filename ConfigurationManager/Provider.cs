fbc17ff8c0bc5bb9dd622fe025941bcfdbde4598
Lirosk <62136946+Lirosk@users.noreply.github.com>
2020-12-14 10:15:35 +0300

    Add files via upload

---
+++ ConfigurationManager/Provider.cs
@@ -0,0 +1,20 @@
+﻿namespace ConfigurationManager
+{
+	//as Artsiom said
+	public class Provider
+	{
+		private readonly Parsers.IParser _configParser;
+
+		public T GetConfig<T>(string filePath)
+		{
+			return _configParser.Parse<T>(filePath);
+		}
+
+		public Provider(Parsers.IParser configParser)
+		{
+			_configParser = configParser;
+		}
+
+		public Provider() { }
+	}
+}
