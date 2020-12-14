05266abfbfb4530bc21447b0ed3f0a4b10eb0e4a
Lirosk <62136946+Lirosk@users.noreply.github.com>
2020-12-14 10:44:32 +0300

    Update OneCityDeals.sql

--- Examples/StoredProcedure/OneCityDeals.sql
+++ Examples/StoredProcedure/OneCityDeals.sql
@@ -10,17 +10,17 @@ ALTER procedure [dbo].[OneCityDeals]
 @Beginning_Date datetime, @Ending_Date datetime
 as
 select
-Customers.CustomerID as CustomerId,
-Customers.ContactName as CustomerName,
-Orders.EmployeeID as EmployeeId, 
-Employees.FirstName + ' ' + Employees.LastName as EmployeeName,
-Customers.City, 
-Products.ProductName,
-Products.SupplierID as SupplierId,
-Suppliers.CompanyName
+  Customers.CustomerID as CustomerId,
+  Customers.ContactName as CustomerName,
+  Orders.EmployeeID as EmployeeId, 
+  Employees.FirstName + ' ' + Employees.LastName as EmployeeName,
+  Customers.City, 
+  Products.ProductName,
+  Products.SupplierID as SupplierId,
+  Suppliers.CompanyName
 from Employees 
-inner join Orders on Orders.EmployeeID = Employees.EmployeeID
-inner join Customers on Customers.City = Employees.City
-inner join Suppliers on Suppliers.City = Employees.City
-inner join Products on Products.SupplierID = Suppliers.SupplierID
+  inner join Orders on Orders.EmployeeID = Employees.EmployeeID
+  inner join Customers on Customers.City = Employees.City
+  inner join Suppliers on Suppliers.City = Employees.City
+  inner join Products on Products.SupplierID = Suppliers.SupplierID
 where Orders.ShippedDate Between @Beginning_Date And @Ending_Date
